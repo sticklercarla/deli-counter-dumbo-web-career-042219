5ea3279cb4a1e58888e4a2db3e3f91559a8cb5f7
Carla Stickler <carlastickler@gmail.com>
2019-04-14 21:26:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   other_deli = []
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, i|
-    other_deli.push("The line is currently: #{i + 1}. #{name}")
+    other_deli.push("#{i + 1}. #{name}")
   end
     puts other_deli
   else
