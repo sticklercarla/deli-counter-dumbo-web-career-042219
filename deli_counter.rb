1e7fa6882f5189aa4c2cd8d08bdd1c5f112c8fd6
Carla Stickler <carlastickler@gmail.com>
2019-04-14 21:48:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, i|
     other_deli.push("#{i + 1}. #{name}")
   end
-    puts other_deli << message.first
+    puts other_deli << message
   end
 end
   
