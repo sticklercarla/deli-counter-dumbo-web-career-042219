d5363d1a144a651c609956d9512c1b40f3605466
Carla Stickler <carlastickler@gmail.com>
2019-04-14 21:33:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  other_deli = []
+  other_deli = "The current line is: "
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, i|
     other_deli.push("#{i + 1}. #{name}")
   end
-    puts "The line is currently: " + other_deli
+    puts other_deli
   else
     puts "The line is currently empty."
   end
