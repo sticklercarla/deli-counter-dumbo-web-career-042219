4f3f182d7343151398ce6331f7665dacfad030fc
Carla Stickler <carlastickler@gmail.com>
2019-04-14 22:26:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,9 +20,10 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   other_deli = []
   katz_deli.each_with_index do |name, i|
-    other_deli.push("Welcome, #{name}. You are number #{i + 1} in line.").join
+    other_deli.push("Welcome, #{name}. You are number #{i + 1} in line.")
   end
-  puts other_deli
+  katz_deli_line = other_deli.join
+  puts katz_deli_line
 end
   
   
