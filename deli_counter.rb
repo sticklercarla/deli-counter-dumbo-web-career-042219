2bc0fc773059c1917f8f8cad649625cce7de1ce4
Carla Stickler <carlastickler@gmail.com>
2019-04-14 22:38:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,8 +25,7 @@ def take_a_number(katz_deli, name)
     katz_deli.each_with_index do |name, i|
       other_deli.push("Welcome, #{name}. You are number #{i + 1} in line.")
     end
-    katz_deli_line = other_deli.join
-    puts katz_deli_line
+    katz_deli_line.length
     
  end
 end
