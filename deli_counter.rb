f18322627375d10e233a8e3b620595964b8051cd
Carla Stickler <carlastickler@gmail.com>
2019-04-14 21:12:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,7 @@ def line(katz_deli)
   if katz_deli > []
     katz_deli.each_with_index do |name, i|
     other_deli.push("The line is currently: #{i + 1}. #{name}")
+  end
   else
     "The line is currently empty."
   end
